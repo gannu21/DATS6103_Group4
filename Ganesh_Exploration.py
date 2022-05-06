eff75224d9d4b40b3372de2f8189db68dec614a8
Ganesh <ganeshghimire40@gmail.com>
2022-05-06 15:19:53 -0400

    final edits
    gansh files final edits

--- Ganesh_Exploration.py
+++ Ganesh_Exploration.py
@@ -1,4 +1,5 @@
-#%%
+# %%[markdown]
+# * Necessary Imports
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -12,27 +13,27 @@ from sklearn.pipeline import make_pipeline
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import cross_val_score
 from sklearn import set_config
+import warnings
+warnings.filterwarnings("ignore")
 
 
-#%%
-df1 = pd.read_json('cardata.json')
+
+#%%  ### three dataframe scrapped on three different days. Combining all three files to make final file
+df1 = pd.read_json('midtermwork/cardata.json')
 df1 = df1.drop_duplicates(subset = ['vin'], keep = 'first')
-df2 = pd.read_json('cardata2.json')
+df2 = pd.read_json('midtermwork/cardata2.json')
 df2 = df2.drop_duplicates(subset = ['vin'], keep = 'first')
-#%%
-df3 = pd.read_json('cardata4.json')
+df3 = pd.read_json('midtermwork/cardata4.json')
 df3 = df3.drop_duplicates(subset = ['vin'], keep = 'first')
-
-# %%
 df_final = pd.concat([df1,df2, df3])
 
-# %%   ### Deleting all the duplicates
-## Adding few extra columns
+#  %% [markdown]
+# * Deleting all the duplicates
+# * Adding few extra columns that might help in visualization
 df_final = df_final.drop_duplicates(subset = ['vin'], keep = 'first')
 df_final['PriceDiff'] = df_final['msrp'] - df_final['base_price']
 df_final['yeargroup'] = df_final['year'].apply(lambda x: '<3 years' if x>2019 else '>3 years')
-# %%
-df_final.info()
+print(df_final.info())
 # %% [markdown]
 ### Exploration
 #   * Lets looks at the inventory by make, cylinders and type
@@ -51,7 +52,7 @@ fig.tight_layout()
 plt.show()
 
 
-# %% 
+# %%[markdown]
 # Exploration by Price
 # Currently there are of outliers. MSRP is compared for the price less than 80K
 
@@ -64,17 +65,15 @@ for ax in fig.axes:
     plt.xticks(rotation=90)
 fig.tight_layout()
 plt.show()
-# %%
-# %%
+# %% ### interactive plot that will help in visualization
 fig = px.scatter(df_final[(df_final.msrp<80000) & (df_final.msrp>100) & (df_final.base_price<100000)], 
                 x = 'base_price',
                 y= 'msrp',color = 'make', 
                 hover_name = 'make', 
                 hover_data = ['cylinders', 'trasmission', 'Type', 'mileage'],
-                size = 'mileage', size_max = 20, 
+                size = 'mileage', size_max = 30, 
                 )
 fig.update_layout(title_text='Baseprice vs MSRP', title_x=0.5)
-
 fig.show()
 # %% [markdown]
 #### Looking into car brands with highest selection
@@ -97,20 +96,16 @@ for brand in Brands:
 # %%  segregattign the data for the mdoel building
 data = df_final[df_final.groupby('make')['make'].transform('size') > 1000].drop([ 'stock_Number', 'vin', 'storeName', 'city', 'state', 'exteriorColor', 'interiorColor', 'storezip', 'PriceDiff', 'yeargroup'], axis=1)
 data = data.loc[(data.msrp<80000) & (data.msrp>2000)]
-# %%
 ### further subsetting the data with the models that more more than 20 cars in inventory
 data.dropna(inplace=True)
 data = data.reset_index(drop=True)
-
-#%%   ### plotting the correlation plot
+### plotting the correlation plot
 plt.figure(figsize=(16,8))
 cmap = sns.diverging_palette(230, 20, as_cmap=True)
 mask = np.triu(np.ones_like(data.corr(), dtype=bool))
 sns.heatmap(data.corr(),mask = mask,  annot = True)
 
-
-### take away the baseprice as it has really high correlation and there is no way for new customer to know it. 
-#%%
+### taking away the baseprice as it has really high correlation and there is no way for new customer to know it. 
 ### separating categorical and continious columns. All cols will be used in subsetting new data to feed into the model
 catcols = [ 'make', 'model','body', 'trasmission','engineType','fuelType', 'Type', 'year', 'mpgCity', 'mpgHighway','cylinders','horsepowerRpm', 'engineSize', 'engineTorque', 'engineTorqueRpm' ]
 print(catcols)
@@ -125,12 +120,9 @@ print(Allcols)
 print(len(Allcols))
 
 
-
 X = data[Allcols]
 y = data['msrp']
 
-
-#%%
 for col in data.columns:
     if data[col].nunique()<30:
         print(col)
@@ -138,9 +130,6 @@ for col in data.columns:
         print(data[col].value_counts())
         print('\n')
 
-# %%
-
-# %%
 
 # %%  ### instancing the model
 lm = LinearRegression()
@@ -171,7 +160,7 @@ pipeline.fit(X, y)
 
 #### Model Evaluation   
 make_trained = X.make.unique()   ### getting the list of the unique car make used to make model
-test_data = pd.read_json('newdata.json')  ### reading new file
+test_data = pd.read_json('midtermwork/newdata.json')  ### reading new file
 test_data = test_data.drop_duplicates(subset = 'vin')   ### dropping duplicate files
 test_data = test_data[test_data['make'].isin(make_trained)]  ### ensuring new testing data has only make from trained model
 test_data = test_data[~test_data['vin'].isin(df_final.vin.unique())]   ### ensuring new data doesn't have the 
@@ -200,9 +189,11 @@ sns.residplot('msrp','predicted_price',  test_data)
 # set_config(display = 'diagram')
 # pipeline
 # %%
-
+from sklearn.metrics import r2_score, mean_squared_error
 # %%
-
+print('Printing the r2_score and rmse in the new data')
+print(r2_score(test_data.msrp, test_data.predicted_price))
+print(np.sqrt(mean_squared_error(test_data.msrp, test_data.predicted_price)))
 
 ### github source for the below code is : https://johaupt.github.io/blog/columnTransformer_feature_names.html
 import sklearn
@@ -279,4 +270,5 @@ coef = coef.reset_index().sort_values(by = [0])
 topandlast50 = pd.concat([coef.head(50), coef.tail(50)]).reset_index(drop=True)
 # %%  ### plotting the model coefficient
 topandlast50.plot.barh(x='index', y =0, figsize = (10,20))
+
 # %%
